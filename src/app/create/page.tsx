9d13c431f844913a08fc64353d81a568adbb6387
Alexander Barke <Alexander.Barke@sea.live>
2023-05-14 09:29:20 +0800

    styling the whole app

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -65,9 +65,11 @@ export default async function Create() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <div className="bg-white p-10 rounded-lg shadow-2xl w-3/4 md:w-1/2 lg:w-3/5">
       <CreateForm onSaveWinner={addResult} />
       <WinnerList onRequestWinners={getWinners} onDelete={deleteWinner} />
+      </div>
     </main>
   );
 }
