af22d36cf1f2a6f1b3c8098e7546889d8d376889
Alexander Barke <Alexander.Barke@sea.live>
2023-05-14 08:49:36 +0800

    clerk sign in sign up

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <SignIn />
+      </div>
+    </main>
+  );
+}
