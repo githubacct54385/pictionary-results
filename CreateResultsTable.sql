5e573bbbe376608a15a76a90691a710c69563beb
Alexander Barke <Alexander.Barke@sea.live>
2023-06-12 12:40:36 +0800

    remove unused things

--- CreateResultsTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Results (
-    id UUID PRIMARY KEY, 
-    datetime TIMESTAMP not null, 
-    animal VARCHAR(255) not null, 
-    winner VARCHAR(255) not null, 
-    artist VARCHAR(255) not null,
-    userId VARCHAR(255) not null,
-    isDeleted bool not null)
